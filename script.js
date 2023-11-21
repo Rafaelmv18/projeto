0654c19b852e0645c55db806b65397358e6776dc
Rafael Venas <rafavenas@gmail.com>
2023-11-21 10:36:20 -0300

    🎉 initial commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function toggleMode() {
   const html = document.documentElement
 
-  //  if (html.classList.contains("dark")) {
-  //    html.classList.remove("dark")
-  //  } else {
-  //    html.classList.add("dark")
-  //}
-  html.classList.toggle("dark")
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark")
+  } else {
+    html.classList.add("dark")
+  }
+  //html.classList.toggle("dark")
 }
